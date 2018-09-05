8160c13904c29e4d32ea36967814a7489cfdcaea
LGouellec <sylvain.legouellec@hotmail.fr>
2018-09-05 18:06:17 +0200

    build

---
+++ src/NestWrapper/build.cmd
@@ -0,0 +1,5 @@
+dotnet restore
+dotnet build
+dotnet pack --no-build --ouput nupkgs
+dotnet nuget push nupkgs\*.nupkg --api-key oy2k54q4szxb3att3huqg276tbseb4cu5ibsk4t5cy7wfq --source https://www.nuget.org/
+del nupkgs\* /F
