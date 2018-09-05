a70a45dc7b5a9d238aab26d709b1112c78625120
LGouellec <sylvain.legouellec@hotmail.fr>
2018-09-05 18:21:09 +0200

    add information in nupkg

--- src/NestWrapper/build.cmd
+++ src/NestWrapper/build.cmd
@@ -1,5 +1,6 @@
+del nupkgs\* /F /Q
 dotnet restore
 dotnet build
-dotnet pack --no-build --ouput nupkgs
+dotnet pack --no-build --output nupkgs
 dotnet nuget push nupkgs\*.nupkg --api-key oy2k54q4szxb3att3huqg276tbseb4cu5ibsk4t5cy7wfq --source https://www.nuget.org/
-del nupkgs\* /F
+del nupkgs\* /F /Q
