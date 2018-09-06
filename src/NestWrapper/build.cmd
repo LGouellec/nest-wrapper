0aed4ca243baa8227c39b98deb7a180b0fc2ec6a
LGouellec <sylvain.legouellec@hotmail.fr>
2018-09-06 09:13:26 +0200

    remove build command

--- src/NestWrapper/build.cmd
+++
@@ -1,6 +0,0 @@
-del nupkgs\* /F /Q
-dotnet restore
-dotnet build
-dotnet pack --no-build --output nupkgs
-dotnet nuget push nupkgs\*.nupkg --api-key oy2k54q4szxb3att3huqg276tbseb4cu5ibsk4t5cy7wfq --source https://www.nuget.org/
-del nupkgs\* /F /Q
